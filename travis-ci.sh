d2d37decf1342c10a7f49a8fda59c4c04dea6ee3
Nicolas Trangez <ikke@nicolast.be>
2013-11-06 00:21:28 +0100

    Add TravisCI build files
    Mostly stolen from @avsm/ocaml-cohttp and slightly altered to suit my needs.

---
+++ travis-ci.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -xue
+
+OPAM_DEPENDS="ocplib-endian lwt core_bench"
+
+if test ! -f setup.ml; then
+    OPAM_DEPENDS="oasis ${OPAM_DEPENDS}"
+fi
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+opam init
+opam install ${OPAM_DEPENDS}
+
+eval `opam config env`
+
+if test ! -f setup.ml; then
+    echo "Not on a release branch, generating Oasis files"
+    oasis setup
+fi
+
+ocaml setup.ml -configure --enable-lwt --enable-demo --enable-benchmark
+make
+
+./demo.byte
